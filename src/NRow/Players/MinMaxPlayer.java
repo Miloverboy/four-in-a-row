6adfbcfa18bfe375511bb08dded267ef1ca041b0
Baraa-k <82050886+Baraa-k@users.noreply.github.com>
2022-10-06 00:28:58 +0200

    minMax + madeMoves

--- src/NRow/Players/MinMaxPlayer.java
+++ src/NRow/Players/MinMaxPlayer.java
@@ -1,16 +1,87 @@
 package NRow.Players;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import NRow.Board;
 import NRow.Heuristics.Heuristic;
+import NRow.Heuristics.SimpleHeuristic;
+import NRow.Node;
+import NRow.State;
+
 
 public class MinMaxPlayer extends PlayerController {
     private int depth;
+    public int move; 
 
     public MinMaxPlayer(int playerId, int gameN, int depth, Heuristic heuristic) {
         super(playerId, gameN, heuristic);
         this.depth = depth;
         //You can add functionality which runs when the player is first created (before the game starts)
     }
+
+    public int max(Board board, int depth){
+        int maxValue = 0;
+        Node node = new Node(new State(board,1), null);
+        node.createChildren();
+        List<Node> children = new ArrayList<>();
+        children.addAll(node.getChildren());
+        if(depth == 0){
+            // if we reached the final layer we wanna reach, we make simple eval and return the best one
+            Heuristic simple = new SimpleHeuristic(gameN);
+            for (Node n: children) {
+                int eval = simple.evaluateBoard(1, n.getState().getBoard());
+                if (eval > maxValue){
+                        maxValue = eval; 
+                    }
+                }
+        
+            return maxValue;
+        }
+        else {
+            //if there are still more depths to discover we make a move based on what the minimizer would ideally do in his next move
+            //based on all the possible moves we have 
+            for (Node n: children) {
+                int eval = min(n.getState().getBoard(), depth-1);
+                if (eval > maxValue){
+                    maxValue = eval;
+                }
+            }
+            return maxValue;
+       }
+}
+
+
+public int min(Board board, int depth){
+    int minValue = 9999;
+    Node node = new Node(new State(board,2), null);
+    node.createChildren();
+    List<Node> children = new ArrayList<>();
+    children.addAll(node.getChildren());
+    if(depth == 0){
+        // if we reached the final layer we wanna reach, we make simple eval and return the best one
+        Heuristic simple = new SimpleHeuristic(gameN);
+        for (Node n: children) {
+            int eval = simple.evaluateBoard(1, n.getState().getBoard());
+            if (eval < minValue){
+                    minValue = eval; 
+                }
+            }
+    
+        return minValue;
+    }
+    else {
+        //if there are still more depths to discover we make a move based on what the maximizer would ideally do in his next move 
+        //based on all the possible moves we have 
+        for (Node n: children) {
+            int eval = max(n.getState().getBoard(), depth-1);
+            if (eval < minValue){
+                minValue = eval;
+            }
+        }
+        return minValue;
+   }
+}
 
     /**
    * Implement this method yourself!
@@ -24,21 +95,49 @@ public class MinMaxPlayer extends PlayerController {
         // HINT: use the functions on the 'heuristic' object to produce evaluations for the different board states!
         
         // Example: 
-        int maxValue = Integer.MIN_VALUE;
         int maxMove = 0;
-        for(int i = 0; i < board.width; i++) { //for each of the possible moves
-            if(board.isValid(i)) { //if the move is valid
-                Board newBoard = board.getNewBoard(i, playerId); // Get a new board resulting from that move
-                int value = heuristic.evaluateBoard(playerId, newBoard); //evaluate that new board to get a heuristic value from it
-                if(value > maxValue) {
-                    maxMove = i;
+        int depth = 4;
+        int maxValue =0;
+        int counter =0;
+        Node node = new Node(new State(board,0), null);
+        node.createChildren();
+        List<Node> children = new ArrayList<>();
+        children.addAll(node.getChildren());
+        //Create children of a node based on the current board
+        // If no moves have been made, then it means that player 1 (the maximiser will start)
+        if (board.getMovesCount() == 0){
+            for(Node n: children){
+                //check for the best child and make the move based that
+                int eval= min(n.getState().getBoard(),depth);
+                if (eval>maxValue){
+                    eval=maxValue; 
+                    maxMove=counter;
                 }
+                counter++;
             }
         }
-        // This returns the same as:
-        heuristic.getBestAction(playerId, board); // Very useful helper function!
-        
-
+        // If an even number of moves have been made, then it means that player 1 (the maximiser will play a move)
+        else if (board.getMovesCount() % 2 == 0){
+            for(Node n: children){
+                int eval= min(n.getState().getBoard(),depth);
+                if (eval>maxValue){
+                    eval=maxValue; 
+                    maxMove=counter;
+                }
+                counter++;
+            }
+        }
+        else{
+            // If an odd number of moves have been made, then it means that player 2 (the minimiser will play a move)
+            for(Node n: children){
+                int eval= max(n.getState().getBoard(),depth);
+                if (eval>maxValue){
+                    eval=maxValue; 
+                    maxMove=counter;
+                }
+                counter++;
+            }
+        }
         /*
         This is obviously not enough (this is depth 1)
         Your assignment is to create a data structure (tree) to store the gameboards such that you can evaluate a higher depths.
